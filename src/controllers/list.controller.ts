f299a6504646a5314af9f107aecb73412dafa7be
Hammad <hammadazmrauf@gmail.com>
2024-01-23 22:40:22 +0500

    add delete & update api

--- src/controllers/list.controller.ts
+++ src/controllers/list.controller.ts
@@ -1,20 +1,41 @@
 /* eslint-disable */
 
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Delete, Param, Patch, Body } from "@nestjs/common";
 import { ListService } from "src/service/list.service";
 import { HttpStatus } from "@nestjs/common";
+import { ListDto } from "src/dto/list.dto";
 
 interface ListRespose {
     message: string;
     status: HttpStatus;
     list: any[];
 }
+
+interface ListDeleteResponse {
+    message: string;
+    status: HttpStatus;
+}
+
+interface updateResponse {
+    message: string;
+    status: HttpStatus;
+}
 @Controller('list')
 export class ListController {
     constructor(private readonly listService: ListService){}
 
-    @Get('listData')
+    @Get()
     async getList(): Promise<ListRespose> {
         return this.listService.getListData();
+    }
+
+    @Delete(':id')
+    async deleteList(@Param('id') id: string) : Promise<ListDeleteResponse>{
+       return this.listService.deleteListItem(id);
+    }
+
+    @Patch('update/:id')
+    async updateList(@Param('id') id: string, @Body() list: ListDto) : Promise<updateResponse> {
+        return this.listService.updateListItem(id, list)
     }
 }
--- src/service/list.service.ts
+++ src/service/list.service.ts
@@ -5,11 +5,22 @@ import { Model } from 'mongoose';
 import { InjectModel } from "@nestjs/mongoose";
 import { List } from "src/interface/list.interface";
 import { ListModal } from "src/schema/list.model";
+import { ListDto } from "src/dto/list.dto";
 
 interface ListRespose {
     message: string;
     status: HttpStatus;
     list: any[];
+}
+
+interface ListDeleteResponse {
+   message: string;
+   status: HttpStatus;
+}
+
+interface updateResponse {
+   message: string;
+   status: HttpStatus;
 }
 
 @Injectable()
@@ -21,6 +32,32 @@ export class ListService {
         return { message: 'List retrieved successfully', status: HttpStatus.OK, list: listData };
      }catch(error) {
         return { message: error, status: HttpStatus.INTERNAL_SERVER_ERROR, list: [] }
+     }
+    }
+
+    async deleteListItem(id: string): Promise<ListDeleteResponse> {
+      try{
+         const deleteResult = await this.listModel.findByIdAndDelete(id).exec();
+         if (deleteResult) {
+            return { message: 'List item deleted successfully', status: HttpStatus.OK };
+        } else {
+            return { message: 'List item not found', status: HttpStatus.NOT_FOUND };
+        }
+      }catch(error) {
+         return { message: error, status: HttpStatus.INTERNAL_SERVER_ERROR };
+      }
+    }
+
+    async updateListItem(id: string, list: ListDto) : Promise<updateResponse> {
+     try{
+      const updatedList = await this.listModel.findByIdAndUpdate(id, list, { new: true }).exec();
+      if (updatedList) {
+         return { message: 'List item updated successfully', status: HttpStatus.OK };
+     } else {
+         return { message: 'List item not found', status: HttpStatus.NOT_FOUND };
+     }
+     }catch(error) {
+      return { message: error, status: HttpStatus.INTERNAL_SERVER_ERROR };
      }
     }
 }
